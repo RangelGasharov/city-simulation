8ca14e12554147da91d3fcc04a6c7c473d348488
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-08-06 13:24:54 +0200

    Added functionality for importing models, added models folder to the cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,7 @@
-#include "headers/Mesh.h"
+#include "headers/Model.h"
 
 const unsigned int width = 1600;
 const unsigned int height = 1200;
-
-Vertex vertices[] =
-    {
-        Vertex{glm::vec3(-1.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.0f, 0.0f)},
-        Vertex{glm::vec3(-1.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.0f, 1.0f)},
-        Vertex{glm::vec3(1.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(1.0f, 1.0f)},
-        Vertex{glm::vec3(1.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(1.0f, 0.0f)}};
-
-GLuint indices[] =
-    {
-        0, 1, 2,
-        0, 2, 3};
-
-Vertex lightVertices[] =
-    {
-        Vertex{glm::vec3(-0.1f, -0.1f, 0.1f)},
-        Vertex{glm::vec3(-0.1f, -0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f, -0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f, -0.1f, 0.1f)},
-        Vertex{glm::vec3(-0.1f, 0.1f, 0.1f)},
-        Vertex{glm::vec3(-0.1f, 0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f, 0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f, 0.1f, 0.1f)}};
-
-GLuint lightIndices[] =
-    {
-        0, 1, 2,
-        0, 2, 3,
-        0, 4, 7,
-        0, 7, 3,
-        3, 7, 6,
-        3, 6, 2,
-        2, 6, 5,
-        2, 5, 1,
-        1, 5, 4,
-        1, 4, 0,
-        4, 5, 6,
-        4, 6, 7};
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
@@ -71,36 +33,14 @@ int main()
     gladLoadGL();
     glViewport(0, 0, width, height);
 
-    Texture textures[]{
-        Texture(TEXTURE_DIR "/planks.png", "diffuse", 0, GL_RGBA, GL_UNSIGNED_BYTE),
-        Texture(TEXTURE_DIR "/planksSpec.png", "specular", 1, GL_RED, GL_UNSIGNED_BYTE)};
-
     Shader shaderProgram(SHADER_DIR "/default.vert", SHADER_DIR "/default.frag");
-
-    std::vector<Vertex> verts(vertices, vertices + sizeof(vertices) / sizeof(Vertex));
-    std::vector<GLuint> ind(indices, indices + sizeof(indices) / sizeof(GLuint));
-    std::vector<Texture> tex(textures, textures + sizeof(textures) / sizeof(Texture));
-    Mesh floor(verts, ind, tex);
-
-    Shader lightShader(SHADER_DIR "/light.vert", SHADER_DIR "/light.frag");
-    std::vector<Vertex> lightVerts(lightVertices, lightVertices + sizeof(lightVertices) / sizeof(Vertex));
-    std::vector<GLuint> lightInd(lightIndices, lightIndices + sizeof(lightIndices) / sizeof(GLuint));
-    Mesh light(lightVerts, lightInd, tex);
 
     glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
     glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
     glm::mat4 lightModel = glm::mat4(1.0f);
     lightModel = glm::translate(lightModel, lightPos);
 
-    glm::vec3 objectPos = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::mat4 objectModel = glm::mat4(1.0f);
-    objectModel = glm::translate(objectModel, objectPos);
-
-    lightShader.Activate();
-    glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
-    glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
     shaderProgram.Activate();
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(objectModel));
     glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
     glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
@@ -109,6 +49,8 @@ int main()
     Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
     glfwSetWindowUserPointer(window, &camera);
     glfwSetScrollCallback(window, Camera::ScrollCallback);
+
+    Model model(MODEL_DIR "/bunny/scene.gltf");
 
     float deltaTime = 0.0f;
     float lastFrame = 0.0f;
@@ -126,15 +68,13 @@ int main()
         camera.Inputs(window, deltaTime);
         camera.updateMatrix(camera.FOV, 0.1f, 100.0f);
 
-        floor.Draw(shaderProgram, camera);
-        light.Draw(lightShader, camera);
+        model.Draw(shaderProgram, camera);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     shaderProgram.Delete();
-    lightShader.Delete();
 
     glfwDestroyWindow(window);
     glfwTerminate();
