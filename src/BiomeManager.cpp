965571bbd4f8b6e76201c56419ddad8568a3a681
RangelGasharov <gasharov@outlook.com>
2026-02-27 23:58:37 +0100

    Made changes to the determination of the climate and biome

--- src/BiomeManager.cpp
+++ src/BiomeManager.cpp
@@ -7,6 +7,7 @@ BiomeManager::BiomeManager(unsigned int seed)
       erosionNoise(seed + 303),
       weirdNoise(seed + 404)
 {
+
     biomes.push_back(Biome("Deep Ocean", {0.0f, 0.05f, 0.3f}, -60.0, 5.0, 4, 0.5, 2.0, 0.5, 0.5, 0.5, -0.8, 0.5, 0.2));
     biomes.push_back(Biome("Ocean", {0.0f, 0.2f, 0.6f}, -25.0, 8.0, 4, 0.5, 2.0, 0.6, 0.5, 0.6, -0.4, 0.4, 0.3));
 
@@ -16,13 +17,13 @@ BiomeManager::BiomeManager(unsigned int seed)
 
     biomes.push_back(Biome("Jungle", {0.0f, 0.3f, 0.0f}, 15.0, 40.0, 8, 0.6, 2.1, 1.1, 0.85, 0.95, 0.2, 0.6, 0.4));
     biomes.push_back(Biome("Sand Desert", {0.9f, 0.8f, 0.4f}, 8.0, 10.0, 4, 0.5, 2.0, 0.7, 1.0, 0.05, 0.3, 0.2, 0.3));
-    biomes.push_back(Biome("Badlands", {0.8f, 0.4f, 0.2f}, 20.0, 60.0, 8, 0.6, 2.0, 0.7, 0.9, 0.25, 0.4, 0.7, 0.5));
+    biomes.push_back(Biome("Badlands", {0.8f, 0.4f, 0.2f}, 20.0, 60.0, 8, 0.6, 2.0, 0.7, 1.0, 0.05, 0.4, 0.3, 0.5));
 
     biomes.push_back(Biome("Tundra", {0.7f, 0.8f, 0.8f}, 5.0, 10.0, 5, 0.5, 2.0, 0.9, 0.2, 0.3, 0.2, 0.5, 0.3));
     biomes.push_back(Biome("Snow Desert", {0.8f, 0.9f, 0.9f}, 8.0, 10.0, 4, 0.5, 2.0, 0.7, 0.1, 0.05, 0.3, 0.2, 0.4));
 
-    biomes.push_back(Biome("Mountains", {0.5f, 0.5f, 0.55f}, 40.0, 450.0, 12, 0.5, 2.4, 1.3, 0.4, 0.4, 0.6, 0.2, 0.8));
-    biomes.push_back(Biome("Snowy Peaks", {1.0f, 1.0f, 1.0f}, 100.0, 650.0, 14, 0.55, 2.5, 1.5, 0.1, 0.3, 0.7, 0.1, 1.0));
+    /* biomes.push_back(Biome("Mountains", {0.5f, 0.5f, 0.55f}, 40.0, 450.0, 12, 0.5, 2.4, 1.3, 0.4, 0.4, 0.6, 0.2, 0.8));
+     biomes.push_back(Biome("Snowy Peaks", {1.0f, 1.0f, 1.0f}, 100.0, 650.0, 14, 0.55, 2.5, 1.5, 0.1, 0.3, 0.7, 0.1, 1.0));*/
 }
 
 // Biome river = Biome("River", {0.2f, 0.4f, 0.8f}, -1.0, 5.0, 4, 0.5, 2.0, 0.5, 0.7, 0.6, -0.5, 0.5, 0.5);
@@ -56,30 +57,38 @@ BiomeManager::ClimatePoint BiomeManager::getClimate(glm::vec3 direction)
 
     double latitude = std::abs(p.y);
     double tempBase = 1.0 - std::pow(latitude, 2.0);
-    double tNoise = tempNoise.noise(p.x * 0.01, p.y * 0.01, p.z * 0.01);
+    double tFreq = 10.0;
+    double tNoise = tempNoise.fractalNoise(p.x * tFreq, p.y * tFreq, p.z * tFreq, 8, 0.5, 2.0);
     double temperature = tempBase * 0.85 + tNoise * 0.15;
 
-    double contRaw = fbm3D(direction * 1.5f, 12, 0.55, contNoise);
+    double contRaw = fbm3D(direction * 1.2f, 8, 0.55, contNoise);
 
     double cont;
     if (contRaw > 0)
     {
-        cont = std::pow(contRaw, 0.2);
+        cont = std::pow(contRaw, 0.45);
+        cont -= 0.1;
     }
     else
     {
-        cont = -std::pow(std::abs(contRaw), 0.2);
+        cont = -std::pow(std::abs(contRaw), 0.25);
+        cont -= 0.15;
     }
-    cont -= 0.1;
 
-    double mRaw = (moistNoise.noise(p.x * 2.5, p.y * 2.5, p.z * 2.5) + 1.0) * 0.5;
+    double mFreq = 2.30;
+    double mRaw = (moistNoise.noise(p.x * mFreq, p.y * mFreq, p.z * mFreq) + 1.0) * 0.5;
+    // double mRaw = (moistNoise.fractalNoise(p.x * mFreq, p.y * mFreq, p.z * mFreq, 8, 0.5, 2.0) + 1.0) * 0.5;
     double moisture = 3.0 * mRaw * mRaw - 2.0 * mRaw * mRaw * mRaw;
 
-    double wRaw = (weirdNoise.noise(p.x * 3.0, p.y * 3.0, p.z * 3.0) + 1.0) * 0.5;
-    double weirdness = std::pow(wRaw, 3.0);
+    double wFreq = 2.80;
+    double wRaw = (weirdNoise.noise(p.x * wFreq, p.y * wFreq, p.z * wFreq) + 1.0) * 0.5;
+    // double wRaw = (weirdNoise.fractalNoise(p.x * wFreq, p.y * wFreq, p.z * wFreq, 8, 0.5, 2.0) + 1.0) * 0.5;
+    double weirdness = std::pow(wRaw, 2.2);
 
-    double eRaw = (erosionNoise.noise(p.x * 2.0, p.y * 2.0, p.z * 2.0) + 1.0) * 0.5;
-    double erosion = std::pow(eRaw, 1.2);
+    double eFreq = 1.80;
+    double eRaw = (erosionNoise.noise(p.x * eFreq, p.y * eFreq, p.z * eFreq) + 1.0) * 0.5;
+    // double eRaw = (erosionNoise.fractalNoise(p.x * eFreq, p.y * eFreq, p.z * eFreq, 8, 0.5, 2.0) + 1.0) * 0.5;
+    double erosion = std::pow(eRaw, 1.1);
 
     return {
         temperature,
@@ -99,11 +108,11 @@ Biome BiomeManager::getBiomeAt(glm::vec3 direction)
 
     for (auto &biome : biomes)
     {
-        double dx = (climate.temperature - biome.temperature) * 5.0;
-        double dy = (climate.moisture - biome.moisture) * 1.5;
-        double dz = (climate.continentalness - biome.continentalness) * 5.0;
-        double de = (climate.erosion - biome.erosion) * 3.0;
-        double dw = (climate.weirdness - biome.weirdness) * 10.0;
+        double dx = (climate.temperature - biome.temperature) * 3.5;
+        double dy = (climate.moisture - biome.moisture) * 1.4;
+        double dz = (climate.continentalness - biome.continentalness) * 4.0;
+        double de = (climate.erosion - biome.erosion) * 2.5;
+        double dw = (climate.weirdness - biome.weirdness) * 8.0;
 
         double d2 = dx * dx + dy * dy + dz * dz + de * de + dw * dw;
         dists2.push_back(d2);
@@ -112,7 +121,7 @@ Biome BiomeManager::getBiomeAt(glm::vec3 direction)
             minSquareDist = d2;
     }
 
-    double blendingSharpness = 24.0;
+    double blendingSharpness = 8.0;
 
     for (size_t i = 0; i < biomes.size(); ++i)
     {
